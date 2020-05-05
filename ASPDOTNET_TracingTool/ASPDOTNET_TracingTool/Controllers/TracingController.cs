4dd9c449d1a6dde4f1b4277aacd51059152218a7
Justin Hagerty <wheelsmanx@gmail.com>
2020-05-05 06:19:28 -0400

    Init

---
+++ ASPDOTNET_TracingTool/ASPDOTNET_TracingTool/Controllers/TracingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Text;
+
+namespace ASPDOTNET_TracingTool.Controllers
+{
+    [Route("")]
+    public class TracingController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+
+            return "sure";
+        }
+
+
+    }
+}
